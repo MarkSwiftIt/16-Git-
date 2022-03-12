9be0e79ed67caa2c251436e7d63d3a9b76b71792
MarkSwiftIt <markgocharovit@gmail.com>
2022-03-12 03:43:07 +0300

    1

---
+++ 16(Git)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  16(Git)
+//
+//  Created by Mark Goncharov on 12.03.2022.
+//
+
+import UIKit
+
+class ViewControllers: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
